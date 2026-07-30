f0d75e27ff380828bd00ec3a7bbdcf547dfee438
Kshitij Mishra <7787.kshitijmishra@gmail.com>
2026-07-31 01:12:38 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = "";
+        for(char c:s.toCharArray()){
+            if(Character.isLetterOrDigit(c)){
+                str+=c;
+            }
+        }str = str.toLowerCase();
+        int left = 0;
+        int right=str.length()-1;
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right)) return false;
+            left++;
+            right--;
+        }return true;
+    }
+
+}
