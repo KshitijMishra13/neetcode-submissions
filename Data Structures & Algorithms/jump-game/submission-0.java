063188486bdc68d7f81b93c97041e4832fab3384
Kshitij Mishra <7787.kshitijmishra@gmail.com>
2026-08-05 21:24:31 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max=0;
+        for(int i=0; i<nums.length; i++){
+            if(i>max) return false;
+            int steps = nums[i]+i;
+            max = Math.max(max, steps);
+        }return true;
+    }
+}
